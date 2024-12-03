d342ce0c9d64607d4cc25651d09ad4aa99222bf7
Cristi Bleotiu <cristibleotiu@gmail.com>
2024-12-03 15:59:53 +0200

    feat: moved epoch manager and release app ngrok plugins to admin pipeline

--- constants.py
+++ constants.py
@@ -1,7 +1,18 @@
-# placeholder for edge-node constants
+from naeural_core.constants import ADMIN_PIPELINE
+import os
 
-PROBABLY_NOTHING = 1.618
 
+ADMIN_PIPELINE = {
+  **ADMIN_PIPELINE,
+  'EPOCH_MANAGER_01': {
+    "NGROK_EDGE_LABEL": os.environ.get("EE_NGROK_EDGE_LABEL_EPOCH_MANAGER"),
+    "PROCESS_DELAY": 0,
+  },
+
+  "NAEURAL_RELEASE_APP": {
+    "NGROK_EDGE_LABEL": os.environ.get("EE_NGROK_EDGE_LABEL_RELEASE_APP"),
+  }
+}
 
 
 if __name__ == '__main__':
