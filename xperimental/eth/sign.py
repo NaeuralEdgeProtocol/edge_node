7713811694247352607208b5912b71f7250615ce
Andrei Ionut Damian <andrei.damian@me.com>
2025-01-15 11:29:20 +0200

    fix: refactor experiment

--- xperimental/eth/sign.py
+++ xperimental/eth/sign.py
@@ -35,23 +35,22 @@ if __name__ == '__main__' :
 
    
   node = "0xai_Amfnbt3N-qg2-qGtywZIPQBTVlAnoADVRmSAsdDhlQ-6"
+  node_eth = eng1.node_address_to_eth_address(node)
+  l.P("Node: {}\nNode Eth: {}".format(node, node_eth))
   epochs = [245, 246, 247, 248, 249, 250]
   epochs_vals = [124, 37, 30, 6, 19, 4]
+  USE_ETH_ADDR = True
   
-  types = ["string", "uint256[]", "uint256[]"]
-  values = [node, epochs, epochs_vals]
+  if USE_ETH_ADDR:  
+    types = ["address", "uint256[]", "uint256[]"]
+    node_data = node_eth
+  else:
+    types = ["string", "uint256[]", "uint256[]"]
+    node_data = node
+    
+  values = [node_data, epochs, epochs_vals]
   
  
   s2 = eng1.eth_sign_message(types, values)
   l.P("Results:\n{}".format(json.dumps(s2, indent=2)))
   l.P("Signature: {}".format(eng1.eth_sign_node_epochs(node, epochs, epochs_vals)))
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
