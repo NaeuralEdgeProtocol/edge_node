edb65aafcda21ff2410e03635298c733c7872899
Andrei Ionut DAMIAN <andrei.damian@me.com>
2025-01-31 11:11:15 +0200

    dAuth patch #2 (#49)
    * chore: inc dev ver
    
    * fix: dAuth issue

--- extensions/business/dauth/dauth_manager.py
+++ extensions/business/dauth/dauth_manager.py
@@ -98,6 +98,8 @@ class DauthManagerPlugin(
 
   def __get_response(self, dct_data: dict):
     """
+    TODO: move __get_response to base as a _get_response method or similar
+    
     Create a response dictionary with the given data.
 
     Parameters
@@ -121,14 +123,19 @@ class DauthManagerPlugin(
         - server_uptime: str
             The time that the current node has been running.
     """
-    str_utc_date = self.datetime.now(self.timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
-    # dct_data['server_id'] = self.node_addr # redundant due to the EE_SENDER
-    dct_data['server_alias'] = self.node_id
-    dct_data['server_version'] = self.ee_ver
-    dct_data['server_time'] = str_utc_date
-    dct_data['server_current_epoch'] = self.__get_current_epoch()
-    dct_data['server_uptime'] = str(self.timedelta(seconds=int(self.time_alive)))
-    self.__sign(dct_data) # add the signature over full data
+    try:
+      str_utc_date = self.datetime.now(self.timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
+      # dct_data['server_id'] = self.node_addr # redundant due to the EE_SENDER
+      dct_data['server_alias'] = self.node_id
+      dct_data['server_version'] = self.ee_ver
+      dct_data['server_time'] = str_utc_date
+      dct_data['server_current_epoch'] = self.__get_current_epoch()
+      dct_data['server_uptime'] = str(self.timedelta(seconds=int(self.time_alive)))
+      self.__sign(dct_data) # add the signature over full data
+    except Exception as e:
+      self.P("Error in `get_response`: {}".format(e), color='r')
+      preexisting_error = dct_data.get('error', "")
+      dct_data['error'] = f"{preexisting_error} - {e}"
     return dct_data
    
   
@@ -151,8 +158,13 @@ class DauthManagerPlugin(
       }      
     }    
     """
-    
-    data = self.process_dauth_request(body)
+    try:
+      data = self.process_dauth_request(body)
+    except Exception as e:
+      self.P("Error processing request: {}".format(e), color='r')
+      data = {
+        'error' : str(e)
+      }
     
     response = self.__get_response({
       **data
