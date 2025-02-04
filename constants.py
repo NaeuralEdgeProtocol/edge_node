1eb753ea98725d5311934d5ac1a71087aab32425
Andrei Ionut Damian <andrei.damian@me.com>
2025-02-04 15:50:50 +0200

    fix: new devnet era

--- constants.py
+++ constants.py
@@ -7,7 +7,7 @@ ADMIN_PIPELINE_FILTER = [
 ]
 
 #############    Era information    ###############
-DEFAULT_GENESYS_EPOCH_DATE = "2025-01-28 20:00:00"      # "2025-02-03 17:00:00" for mainnet
+DEFAULT_GENESYS_EPOCH_DATE = "2025-02-04 12:00:00"      # "2025-02-03 17:00:00" for mainnet
 DEFAULT_EPOCH_INTERVALS =  1                            # 24 mainnet, 1 for devnet
 DEFAULT_EPOCH_INTERVAL_SECONDS = 3600                   # 3600
 SUPERVISOR_MIN_AVAIL_PRC = 0.50                         # 0.98 for mainnet, 60% for testnet
